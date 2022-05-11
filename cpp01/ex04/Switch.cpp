1a20a6534d0f5b797fa87d5cd0813c8d20aa8a3e
Freya-Tenebrae <freya.tenebrae@gmail.com>
2022-05-11 10:56:05 +0200

    cpp01 - 04 remove some comment

--- cpp01/ex04/Switch.cpp
+++ cpp01/ex04/Switch.cpp
@@ -6,7 +6,7 @@
 /*   By: cmaginot <cmaginot@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/09 14:45:33 by cmaginot          #+#    #+#             */
-/*   Updated: 2022/05/11 10:43:34 by cmaginot         ###   ########.fr       */
+/*   Updated: 2022/05/11 10:55:42 by cmaginot         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -84,23 +84,3 @@ void Switch::replacing()
 		this->createNew_file(buffer);
 	}
 }
-
-// https://www.cplusplus.com/reference/fstream/ifstream/
-// https://www.cplusplus.com/reference/string/string/
-
-// std::ifstream ifs("file_in")
-// std::ofstream ofs("file_out")
-
-
-// Create a program that takes three parameters in the following order: 
-// a filename and two strings, s1 and s2.
-
-// It will open the file <filename> and copies its content into a new file
-// <filename>.replace, replacing every occurrence of s1 with s2.
-
-// Using C file manipulation functions is forbidden and will be considered 
-// cheating. All the member functions of the class std::string are allowed, 
-// except replace. Use them wisely!
-
-// Of course, handle unexpected inputs and errors. You have to create and 
-// turn in your own tests to ensure your program works as expected.
