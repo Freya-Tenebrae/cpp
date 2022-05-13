68dd8466dd89bfcdb7c92157bc8ddab92b8f80b6
Celien MAGINOT <cmaginot@student.42.fr>
2022-05-13 19:07:51 +0200

    cpp02 - 00 minor modification

--- cpp02/ex00/Fixed.hpp
+++ cpp02/ex00/Fixed.hpp
@@ -6,7 +6,7 @@
 /*   By: cmaginot <cmaginot@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/13 15:47:29 by cmaginot          #+#    #+#             */
-/*   Updated: 2022/05/13 16:07:15 by cmaginot         ###   ########.fr       */
+/*   Updated: 2022/05/13 16:58:07 by cmaginot         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -30,23 +30,7 @@ class Fixed
 		int		getRawBits(void) const;
 		void	setRawBits(int const raw);
 		
-		Fixed &operator=(const Fixed &f);
+		Fixed	&operator=(const Fixed &f);
 };//!Fixed
 
 #endif
-
-// • Membres prive :
-// Un entier pour stocker la valeur du nombre en virgule fixe.
-// ◦ Un entier constant statique pour stocker le nombre de bits de la partie
-// fractionnaire, et dont la valeur sera toujours le littéral entier 8.
-
-// • Membres publics :
-// ◦ Un constructeur par défaut qui initialisera la valeur du nombre à virgule fixe
-// à 0.
-// ◦ Un constructeur de recopie.
-// ◦ Une surcharge de l’opérateur d’affectation.
-// ◦ Un destructeur.
-// ◦ Une fonction membre int getRawBits( void ) const;
-// qui retourne la valeur du nombre à virgule fixe sans la convertir.
-// ◦ Une fonction membre void setRawBits( int const raw );
-// qui initialise la valeur du nombre à virgule fixe avec celle passée en paramètre.
