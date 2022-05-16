b889b37160a272c7384248bef4e0043191e65cdb
Celien MAGINOT <cmaginot@student.42.fr>
2022-05-16 18:34:33 +0200

    cpp02 - 02 done

---
+++ cpp02/ex02/Fixed.cpp
@@ -0,0 +1,216 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Fixed.cpp                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: cmaginot <cmaginot@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/05/13 15:47:29 by cmaginot          #+#    #+#             */
+/*   Updated: 2022/05/16 18:13:48 by cmaginot         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "Fixed.hpp"
+
+/* ************************************************************************** */
+/* CONSTRUCTOR & DESTRUCTOR                                                   */
+/* ************************************************************************** */
+
+Fixed::Fixed():_rawBits(0)
+{
+	// std::cout << "\033[0;35mDefault constructor called\033[0m" << std::endl;
+}
+
+Fixed::Fixed(const Fixed &f)
+{
+	// std::cout << "\033[0;35mCopy constructor called\033[0m" << std::endl;
+	(*this) = f;
+}
+
+Fixed::Fixed(int const raw)
+{
+	// std::cout << "\033[0;35mInt constructor called\033[0m" << std::endl;
+	_rawBits = raw << _number;
+}
+
+Fixed::Fixed(float const raw)
+{
+	// std::cout << "\033[0;35mFloat constructor called\033[0m" << std::endl;
+	_rawBits = roundf(raw * (1 << _number));
+}
+
+
+Fixed::~Fixed()
+{
+	// std::cout << "\033[0;35mDestructor called\033[0m" << std::endl;
+}
+
+/* ************************************************************************** */
+/* GETTER & SETTER                                                            */
+/* ************************************************************************** */
+
+int	Fixed::getRawBits(void) const
+{
+	return (_rawBits);
+}
+
+void	Fixed::setRawBits(int const raw)
+{
+	_rawBits = raw;
+}
+
+/* ************************************************************************** */
+/* MEMBER FUNCTION                                                            */
+/* ************************************************************************** */
+
+int		Fixed::toInt(void) const
+{
+	return (_rawBits >> _number);
+}
+
+float	Fixed::toFloat(void) const
+{
+	return (_rawBits / (float)(1 << _number));
+}
+
+Fixed	&Fixed::min(Fixed &f1, Fixed &f2)
+{
+	if (f1.getRawBits() <= f2.getRawBits())
+		return (f1);
+	else
+		return (f2);
+}
+
+Fixed const	&Fixed::min(const Fixed &f1, const Fixed &f2)
+{
+	if (f1.getRawBits() <= f2.getRawBits())
+		return (f1);
+	else
+		return (f2);
+}
+
+Fixed	&Fixed::max(Fixed &f1, Fixed &f2)
+{
+	if (f1.getRawBits() >= f2.getRawBits())
+		return (f1);
+	else
+		return (f2);
+}
+
+Fixed const	&Fixed::max(const Fixed &f1, const Fixed &f2)
+{
+	if (f1.getRawBits() >= f2.getRawBits())
+		return (f1);
+	else
+		return (f2);
+}
+
+/* ************************************************************************** */
+/* OPERATOR OVERLOADING                                                       */
+/* ************************************************************************** */
+
+Fixed &Fixed::operator=(const Fixed &f)
+{
+	// std::cout << "\033[0;35mCopy assignment operator called\033[0m";
+	// std::cout << std::endl;
+	this->setRawBits(f.getRawBits());
+	return (*this);
+}
+
+bool const	Fixed::operator>(const Fixed &f)
+{
+	return (_rawBits > f.getRawBits());
+}
+
+bool const	Fixed::operator<(const Fixed &f)
+{
+	return (_rawBits < f.getRawBits());
+}
+
+bool const	Fixed::operator>=(const Fixed &f)
+{
+	return (_rawBits >= f.getRawBits());
+}
+
+bool const	Fixed::operator<=(const Fixed &f)
+{
+	return (_rawBits <= f.getRawBits());
+}
+
+bool const	Fixed::operator==(const Fixed &f)
+{
+	return (_rawBits == f.getRawBits());
+}
+
+bool const	Fixed::operator!=(const Fixed &f)
+{
+	return (_rawBits != f.getRawBits());
+}
+
+Fixed	Fixed::operator+(const Fixed &f)
+{
+	Fixed	fthiscpy(*this);
+
+	fthiscpy.setRawBits(_rawBits + f.getRawBits());
+	return (fthiscpy);
+}
+
+Fixed	Fixed::operator-(const Fixed &f)
+{
+	Fixed	fthiscpy(*this);
+
+	fthiscpy.setRawBits(_rawBits - f.getRawBits());
+	return (fthiscpy);
+}
+
+Fixed	Fixed::operator*(const Fixed &f)
+{
+	Fixed	fthiscpy(*this);
+	float	raw1 = toFloat();
+	float	raw2 = f.toFloat();
+
+	fthiscpy.setRawBits(roundf((raw1 * raw2) * (1 << _number)));
+	return (fthiscpy);
+}
+
+Fixed	Fixed::operator/(const Fixed &f)
+{
+	Fixed	fthiscpy(*this);
+	float	raw1 = toFloat();
+	float	raw2 = f.toFloat();
+
+	fthiscpy.setRawBits(roundf((raw1 / raw2) * (1 << _number)));
+	return (fthiscpy);
+}
+
+Fixed	Fixed::operator++()
+{
+	_rawBits++;
+	return(*this);
+}
+
+Fixed	Fixed::operator++(int)
+{
+	Fixed tmp(*this);
+	_rawBits++;
+	return(tmp);
+}
+
+Fixed	Fixed::operator--()
+{
+	_rawBits--;
+	return(*this);
+}
+
+Fixed	Fixed::operator--(int)
+{
+	Fixed tmp(*this);
+	_rawBits--;
+	return(tmp);
+}
+
+std::ostream	&operator<<(std::ostream &output, const Fixed &f)
+{
+	output << f.toFloat();
+	return (output);	
+}
---
+++ cpp02/ex02/main.cpp
@@ -0,0 +1,97 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: cmaginot <cmaginot@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/05/16 17:02:54 by cmaginot          #+#    #+#             */
+/*   Updated: 2022/05/16 18:32:35 by cmaginot         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "Fixed.hpp"
+
+int main(int argc, char const *argv[])
+{
+	Fixed a;
+	Fixed const b( Fixed(5.05f) * Fixed(2) );
+	Fixed c(42.75f);
+	Fixed d(10);
+	Fixed e = c;
+	Fixed const f(42.42f);
+	Fixed const g(72.5f);
+
+	std::cout << "\033[1;36mVariables :\033[0m" << std::endl;
+	std::cout << "a        : " << a << std::endl;
+	std::cout << "b        : " << b << std::endl;
+	std::cout << "c        : " << c << std::endl;
+	std::cout << "d        : " << d << std::endl;
+	std::cout << "e        : " << e << std::endl;
+	std::cout << "f        : " << f << std::endl;
+	std::cout << "g        : " << g << std::endl;
+	std::cout << std::endl;
+	std::cout << "\033[1;36mMendatory tests :\033[0m" << std::endl;
+	std::cout << a << std::endl;
+	std::cout << ++a << std::endl;
+	std::cout << a << std::endl;
+	std::cout << a++ << std::endl;
+	std::cout << a << std::endl;
+	std::cout << b << std::endl;
+	std::cout << Fixed::max( a, b ) << std::endl;
+	std::cout << std::endl;
+	std::cout << "\033[1;36mComparison operator tests :\033[0m" << std::endl;
+	std::cout << "c < d    : " << (c < d) << std::endl;
+	std::cout << "c < e    : " << (c < e) << std::endl;
+	std::cout << "c <= d   : " << (c <= d) << std::endl;
+	std::cout << "c <= e   : " << (c <= e) << std::endl;
+	std::cout << "c > d    : " << (c > d) << std::endl;
+	std::cout << "c > e    : " << (c > e) << std::endl;
+	std::cout << "c >= d   : " << (c >= d) << std::endl;
+	std::cout << "c >= e   : " << (c >= e) << std::endl;
+	std::cout << "c == d   : " << (c == d) << std::endl;
+	std::cout << "c == e   : " << (c == e) << std::endl;
+	std::cout << "c != d   : " << (c != d) << std::endl;
+	std::cout << "c != e   : " << (c != e) << std::endl;
+	std::cout << std::endl;
+	std::cout << "\033[1;36mArithmetic operator tests :\033[0m" << std::endl;
+	std::cout << "c + d    : " << (c + d) << std::endl;
+	std::cout << "c + e    : " << (c + e) << std::endl;
+	std::cout << "c - d    : " << (c - d) << std::endl;
+	std::cout << "c - e    : " << (c - e) << std::endl;
+	std::cout << "c * d    : " << (c * d) << std::endl;
+	std::cout << "c * e    : " << (c * e) << std::endl;
+	std::cout << "c / d    : " << (c / d) << std::endl;
+	std::cout << "c / e    : " << (c / e) << std::endl;
+	std::cout << std::endl;
+	std::cout << "\033[1;36mIncremetation operator tests :\033[0m" << std::endl;
+	std::cout << "c++      : " << c++ << "   --> " << c << std::endl;
+	std::cout << "d++      : " << d++ << "      --> " << d << std::endl;
+	std::cout << "++c      : " << ++c << " --> " << c << std::endl;
+	std::cout << "++d      : " << ++d << " --> " << d << std::endl;
+	std::cout << "c--      : " << c-- << " --> " << c << std::endl;
+	std::cout << "d--      : " << d-- << " --> " << d << std::endl;
+	std::cout << "--c      : " << --c << "   --> " << c << std::endl;
+	std::cout << "--d      : " << --d << "      --> " << d << std::endl;
+	std::cout << std::endl;
+	std::cout << "\033[1;36mMax and Min function tests :\033[0m" << std::endl;
+	std::cout << "max(c,d) : " << Fixed::max(c, d) << std::endl;
+	std::cout << "max(c,e) : " << Fixed::max(c, e) << std::endl;
+	std::cout << "max(f,g) : " << Fixed::max(f, g) << std::endl;
+	std::cout << "max(c,g) : " << Fixed::max(c, g) << std::endl;
+	std::cout << "min(c,d) : " << Fixed::min(c, d) << std::endl;
+	std::cout << "min(c,e) : " << Fixed::min(c, e) << std::endl;
+	std::cout << "min(f,g) : " << Fixed::min(f, g) << std::endl;
+	std::cout << "min(c,g) : " << Fixed::min(c, g) << std::endl;
+
+
+	return (0);
+}
+
+// 0
+// 0.00390625
+// 0.00390625
+// 0.00390625
+// 0.0078125
+// 10.1016
+// 10.1016
