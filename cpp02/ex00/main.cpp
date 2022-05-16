07c2d87f23cb9caf5e6b4feaa8c59a78f0b0f20f
Celien MAGINOT <cmaginot@student.42.fr>
2022-05-16 18:34:46 +0200

    cpp02 - 00 minor modification

--- cpp02/ex00/main.cpp
+++ cpp02/ex00/main.cpp
@@ -6,7 +6,7 @@
 /*   By: cmaginot <cmaginot@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/13 15:47:35 by cmaginot          #+#    #+#             */
-/*   Updated: 2022/05/13 16:50:24 by cmaginot         ###   ########.fr       */
+/*   Updated: 2022/05/16 17:18:20 by cmaginot         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,7 +16,7 @@ int main(int argc, char const *argv[])
 {
 
 	Fixed a;
-	Fixed b( a );
+	Fixed b(a);
 	Fixed c;
 	c = b;
 	std::cout << a.getRawBits() << std::endl;
