50f62d125db6938ba836ec4de6b293c77e14fd95
Celia Maginot <cmaginot@e1r8p14.clusters.42paris.fr>
2022-08-11 15:43:37 +0200

    add empty cpp05

---
+++ cpp05/ex00/main.cpp
@@ -0,0 +1,43 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: cmaginot <cmaginot@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/05/16 17:02:54 by cmaginot          #+#    #+#             */
+/*   Updated: 2022/08/11 15:40:58 by cmaginot         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "Bureaucrat.hpp"
+
+void bureaucratTests()
+{
+	std::cout << "\033[1;36mMBureaucrat tests :\033[0m" << std::endl;
+	std::cout << "\033[0;36m    Create objects :\033[0m" << std::endl;
+	std::cout << "TODO" << std::endl;
+	std::cout << std::endl;
+
+	std::cout << "\033[0;36m    tests :\033[0m" << std::endl;
+	std::cout << "TODO" << std::endl;
+	std::cout << std::endl;
+
+	std::cout << "\033[0;36m    Delete objects :\033[0m" << std::endl;
+	std::cout << "TODO" << std::endl;
+	std::cout << std::endl;
+}
+
+int main()
+{
+	bureaucratTests();
+	std::cout << "\033[1;36mEnd of tests\033[0m" << std::endl;
+	return (0);
+}
+
+
+// todo
+/*
+- change IMateriaSources to cpy pointer and not duplicate ?
+- finish ICharacter
+*/
