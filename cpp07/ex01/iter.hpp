c69ce7a4c2881308affd8e3bc37f5b3bddd1f600
Celia Maginot <cmaginot@e1r6p1.clusters.42paris.fr>
2022-09-05 09:51:23 +0200

    correct small detail ex01

--- cpp07/ex01/iter.hpp
+++ cpp07/ex01/iter.hpp
@@ -6,12 +6,12 @@
 /*   By: cmaginot <cmaginot@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/02 20:17:02 by cmaginot          #+#    #+#             */
-/*   Updated: 2022/09/05 08:15:40 by cmaginot         ###   ########.fr       */
+/*   Updated: 2022/09/05 08:26:38 by cmaginot         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef WHATEVER_HPP
-# define WHATEVER_HPP
+#ifndef ITER_HPP
+# define ITER_HPP
 
 # include <string>
 # include <iostream>
